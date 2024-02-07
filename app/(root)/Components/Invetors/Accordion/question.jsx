d46a11e2ca257f9a516ae51621469f538d86239d
Mohammad-abdo <sdm800884@gmail.com>
2024-02-07 22:59:25 +0200

    fix accordion

--- app/(root)/Components/Invetors/Accordion/question.jsx
+++ app/(root)/Components/Invetors/Accordion/question.jsx
@@ -15,17 +15,17 @@ const BasicAccordion = ({ head, info }) => {
   return (
     <div>
       <Accordion expanded={expanded}
-       onChange={handleAccordionChange}className='w-full bg-transparent border-b border-[#ec9823] '>
+       onChange={handleAccordionChange}className='w-full bg-transparent border-b border-[#ec9823] bg_transparent'>
         <AccordionSummary
-          expandIcon={expanded ? <RemoveIcon  className='text-[#BC955A] text-lg md:text-4xl font-semibold bg-transparent'/> : <AddIcon className='text-[#BC955A] text-[20px] md:text-4xl font-semibold' />}
+          expandIcon={expanded ? <RemoveIcon  className='text-[#BC955A] text__color text-lg bg_transparent  md:text-4xl font-semibold bg-transparent'/> : <AddIcon className='text-[#BC955A] text-[20px] md:text-4xl font-semibold' />}
           
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography className="text-[#BC955A] text-[15px] md:text-2xl font-semibold w-full md:mx-8 ">{head}</Typography>
+          <Typography className="text-[#BC955A] bg_transparent text__colortext-[15px] md:text-2xl font-semibold w-full md:mx-8 ">{head}</Typography>
         </AccordionSummary>
-        <AccordionDetails className='border-t border-[#ec9823] '>
-          <Typography className='text-[12px] md:text-xl text-white md:mx-8 py-2 md:py-10'>{info}</Typography>
+        <AccordionDetails className='border-t border-[#ec9823] bg_transparent '>
+          <Typography className='text-[12px] md:text-xl text-white md:mx-8 py-2 md:py-10 bg_transparent'>{info}</Typography>
         </AccordionDetails>
       </Accordion>
     </div>
